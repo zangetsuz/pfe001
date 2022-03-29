62d282dc84d7248fc73d46fde642692787e4d12f
Abdelhamid Nouira <abdou125@gmail.com>
2022-03-29 20:17:05 +0100

    created controller but not working zip ext

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
-
+*/
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -26,3 +26,4 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+Route::get('/',[\App\Http\Controllers\UsersController::class,'import']);
